fb2673b059557fb82113ca6e4a6300e5d433ed67
xcx2309 <canberk.demirkan.extern@atruvia.de>
2023-01-28 22:22:39 +0100

    feat: initial commit

---
+++ src/main/java/de/cdn/ytsbmsinventoryservice/YtSbmsInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package de.cdn.ytsbmsinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtSbmsInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtSbmsInventoryServiceApplication.class, args);
+	}
+
+}
